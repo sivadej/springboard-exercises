03db231e28d997003604af0736d54ffcf565c935
Sivadej K <sbkitchpanich@gmail.com>
2020-01-29 21:28:33 -0900

    removed unnecessary object defs

--- ajax-exercise-giphy-party/app.js
+++ ajax-exercise-giphy-party/app.js
@@ -1,6 +1,7 @@
 console.log("Let's get this party started!");
 
 const API_KEY = 'gArSxUzbKNPJS6r3WDYR35rLJjA6F4sH';
+const API_URL = 'https://api.giphy.com/v1/gifs/search';
 
 const container = document.querySelector('#gifs-container');
 const btnSubmit = document.querySelector('#btn-submit');
@@ -9,9 +10,10 @@ const btnClear = document.querySelector('#btn-clear');
 btnSubmit.addEventListener('click', handleSearchClick);
 btnClear.addEventListener('click', handleBtnClear);
 
-// Use API call to GET single URL from search query
+// -Use API call to GET single URL and alt tag from search query
+// -Display image within container
 async function getGIF(query) {
-	const result = await axios.get('https://api.giphy.com/v1/gifs/search', {
+	const result = await axios.get(API_URL, {
 		params: {
 			api_key: API_KEY,
 			q: query,
@@ -21,17 +23,16 @@ async function getGIF(query) {
 			lang: 'en'
 		}
 	});
-	const imgResult = {
-		url: result.data.data[0].images.original.url,
-		alt: result.data.data[0].title
-	};
-	addImgToContainer(getImgElement(imgResult));
+	const url = result.data.data[0].images.original.url;
+	const alt = result.data.data[0].title;
+    addImgToContainer(getImgElement(url, alt));
 }
 
-function getImgElement(imgObj) {
+// Generate img html element for display
+function getImgElement(url, alt) {
 	const imgElement = document.createElement('IMG');
-	imgElement.setAttribute('alt', imgObj.alt);
-	imgElement.src = imgObj.url;
+	imgElement.setAttribute('alt', alt);
+	imgElement.src = url;
 	return imgElement;
 }
 
